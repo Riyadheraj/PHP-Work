c80ef7a4f8ae04f355c1a54f5ca81369ea02a804
Riyaaptech@ <riyadherajdevria@gmail.com>
2025-04-16 23:00:58 -0700

    PHP work

---
+++ Operators/operators.php
@@ -0,0 +1,52 @@
+<?php
+
+// Arithmetic Operators
+
+// $num1 = 10;
+// $num2 = 20;
+
+// $result = $num1 + $num2;
+
+// var_dump($result);
+
+// Comparision Operators;
+
+// $num1 = 20;
+// $num2 = 10;
+
+// $result = $num1 > $num2;
+
+// var_dump($result);
+
+
+// Logical Operators
+
+// $num1 = 20;
+// $num2 = 10;
+// $num3 = 30;
+
+// $result = ($num1 <- $num2) && ($num3 <- $num2);
+// $result = !($num1 !== $num3);
+
+// var_dump($result);
+
+// Assignment Operators
+
+// $a = 10;
+// $b = 5;
+
+// echo $a. "<br>";
+
+// $a += $b;
+
+// echo $a;
+
+
+// Conditional Operators
+
+// $a = 10;
+// $b = 20;
+
+// $result = ($a < $b) ? "This is True" : "This is false";
+
+// echo $result;
