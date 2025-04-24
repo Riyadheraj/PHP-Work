a2a0f3703f778de6492b8f6b6cd8170cf5be7337
Riyaaptech@ <riyadherajdevria@gmail.com>
2025-04-24 07:54:09 +0500

    loops & switch

---
+++ Loops/loops.php
@@ -0,0 +1,31 @@
+<?php
+
+// For Loop
+
+for($i = 1; $i < 10; $i ++){
+    echo $i . " Hello World <br>";
+}
+
+for($i = 10; $i >= 1; $i --){
+    echo $i . " Hello World <br>";
+}
+
+// While Loop
+
+$a = 1;
+while ($a <=10){
+    echo "this is while loop <br>";
+    $a ++;
+}
+
+// Do while
+
+$b = 1;
+do{
+    echo "hello world <br>";
+    $b ++;
+}
+while($b <=10);
+
+
+?>
---
+++ switch/swtich.php
@@ -0,0 +1,65 @@
+<?php
+
+$language = "PHP 8";
+switch($language){
+    case "C":
+    echo "Your Favourite languageg is C";
+    break;
+
+    case "PHP 8":
+    echo "Your Favourite languageg is PHP 8";
+    break;
+
+    case "C++":
+    echo "Your Favourite languageg is C++";
+    break;
+
+    case "Javascript":
+    echo "Your Favourite languageg is Javascript";
+    break;
+    
+    case "Python":
+    echo "Your Favourite languageg is Python";
+    break;
+
+    default:
+    echo "Language is not found";
+    
+}
+
+$dayName = 5;
+switch($dayName){
+    case 1:
+    echo "<br> This is Monday";
+    break;
+
+    case 2:
+    echo "<br> This is Tuesday";
+    break;
+
+    case 3:
+    echo "<br> This is Wednesday";
+    break;
+
+    case 4:
+    echo "<br> This is Thursday";
+    break;
+    
+    case 5:
+    echo "<br> This is Friday";
+    break;
+
+    case 6:
+    echo "<br> This is Saturday";
+    break;
+
+    case 7:
+    echo "<br> This is Sunday";
+    break;
+
+    default:
+    echo "Day not matched";
+    
+}
+?>
+
