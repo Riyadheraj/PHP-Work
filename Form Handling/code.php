cb552e172e3e6c9fed185c7913d6b7f3bbebba3c
Riyaaptech@ <riyadherajdevria@gmail.com>
2025-04-28 22:53:05 -0700

    new form work

---
+++ Form Handling/code.php
@@ -0,0 +1,8 @@
+<?php
+$con = mysqli_connect("localhost", "root", "", "aptechsfc");
+
+if($con){
+    echo "DB connected";
+}
+
+?>
---
+++ Loops/arrayfunctions.php
@@ -0,0 +1,67 @@
+<?php
+
+// Array Functions
+
+// Sort Array
+
+// $data = ["zain", "areeb", "raheel", "ubaid", "faiza"];
+// sort($data). "<br>";
+
+// foreach($data as $value){
+//     echo $value . "<br>";
+// }
+
+// rsort array
+
+// $data = ["zain", "areeb", "raheel", "ubaid", "faiza"];
+// rsort($data). "<br>";
+
+// foreach($data as $value){
+//     echo $value . "<br>";
+// }
+
+// asort array
+
+// $data1 = ['a' => "zain", 'd'=> "areeb",'c' => "raheel", 'b'=> "ubaid", 'e'=> "faiza"];
+// asort($data1). "<br>";
+
+// foreach($data1 as $key => $value){
+//     echo $key . ":". $value . "<br>";
+// }
+
+// ksort array
+
+// $data1 = ['a' => "zain", 'd'=> "areeb",'c' => "raheel", 'b'=> "ubaid", 'e'=> "faiza"];
+// ksort($data1) . "<br>";
+
+// foreach($data1 as $key => $value){
+//     echo $key . ":" . $value . "<br>";
+// }
+
+// Array-Merge
+
+// $cpism = ["html", "css", "javascript", "bootstrap"];
+// $dism = ["git", "xml", "adv.js", "mysql", "php"];
+
+// $merge = array_merge ($cpism , $dism);
+
+// foreach($merge as $value){
+//     echo $value . "<br>";
+// }
+
+
+// Count array
+$data = ["zain", "areeb", "raheel", "ubaid", "faiza"];
+
+echo count($data) . "<br>";
+
+foreach($data as $value){
+    echo $value . "<br>";
+}
+
+
+// Array Structure
+
+// var_dump
+// print_r
+?>
